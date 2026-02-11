8958d8e1a44f5f94c813250207be20ead33f4336
Anand kumar <anandkumarsaidi2005@gmail.com>
2026-02-11 11:16:29 +0530

    test: added testing routes

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,26 +1,29 @@
 import express, { type Request, type Response } from "express"
 import { prisma } from "./lib/prisma.js"
 const app = express();
-app.use(express());
+app.use(express.json());
 
 app.post("/hooks/catch/:userId/:zapId/", async (req: Request, res: Response)=> {
+    console.log("hii there")
     const zapId : any = req.params.zapId;
     const userId = req.params.userId;
     const body: any = req.body;
     try{
         await prisma.$transaction(async tx => {
+            console.log(JSON.stringify(body));
             const run = await tx.zapRun.create({
                 data: {
                     zapId: zapId,
                     metadata: body
                 }
             })
+            
             await tx.zapRunOutbox.create({
                 data : {
                     zapRunId: run.id
                 }
             })
-        })    
+        })
         res.status(200).json({
             msg: "webhook received"
         })
@@ -29,6 +32,19 @@ app.post("/hooks/catch/:userId/:zapId/", async (req: Request, res: Response)=> {
             msg: "something went wrong"
         })
     }
+})
+
+app.post("/user", async (req: Request, res: Response) => {
+        await prisma.user.create({
+            data: {
+                email: "kumar@gmail.com",
+                name: "kumar",
+                password: "kumar"
+            }
+        })
+        return res.json({
+            msg: "user created successfully"
+        })
 })
 
 app.listen(8080, ()=>{
